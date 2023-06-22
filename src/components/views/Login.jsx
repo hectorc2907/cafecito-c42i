8d9062dfcbeecf56412c9bfbd785ee4fe4b3bc04
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-06-21 23:37:29 -0300

    agregamos validaciones al input del password del login

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -24,12 +24,14 @@ const Login = () => {
               <Form.Control
                 type="email"
                 placeholder="Ingrese un email"
-                {...register('email', {
-                  required:'El email es un dato obligatorio',
-                  pattern:{
-                    value:/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=? ^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a -z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-                    message: 'El email debe tener el siguiente formato mail@dominio.com'
-                  }
+                {...register("email", {
+                  required: "El email es un dato obligatorio",
+                  pattern: {
+                    value:
+                      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=? ^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a -z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                    message:
+                      "El email debe tener el siguiente formato mail@dominio.com",
+                  },
                 })}
               />
               <Form.Text className="text-danger">
@@ -39,7 +41,21 @@ const Login = () => {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                {...register("password", {
+                  required: "El password es obligatorio",
+                  pattern: {
+                    value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                    message:
+                      "El password debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. No puede tener otros símbolos.",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
             </Form.Group>
             <Button variant="primary" type="submit">
               Ingresar
