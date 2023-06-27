75a6a65f3e350ebaed95f89b12e19197e2d324e8
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-06-27 12:42:21 -0300

    agregamos el componente rutas administrador y rutas protegidas con la logica para proteger las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -7,41 +7,43 @@ import Inicio from "./components/views/Inicio";
 import Login from "./components/views/Login";
 import Registro from "./components/views/Registro";
 import DetalleProducto from "./components/views/DetalleProducto";
-import CrearProducto from "./components/views/producto/CrearProducto";
-import EditarProducto from "./components/views/producto/EditarProducto";
-import Administrador from "./components/views/Administrador";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
+import RutasProtegidas from "./components/routes/RutasProtegidas";
+import RutasAdministrador from "./components/routes/RutasAdministrador";
 
 function App() {
-  const usuarioSessionStorage = JSON.parse(sessionStorage.getItem('usuario')) || {}
-  const [usuarioLogueado, setUsuarioLogueado] = useState({usuarioSessionStorage});
+  const usuarioSessionStorage =
+    JSON.parse(sessionStorage.getItem("usuario")) || {};
+  const [usuarioLogueado, setUsuarioLogueado] = useState({
+    usuarioSessionStorage,
+  });
   return (
     <BrowserRouter>
-      <Menu usuarioLogueado={usuarioLogueado} setUsuarioLogueado={setUsuarioLogueado}></Menu>
+      <Menu
+        usuarioLogueado={usuarioLogueado}
+        setUsuarioLogueado={setUsuarioLogueado}
+      ></Menu>
       <Routes>
         <Route exact path="/" element={<Inicio></Inicio>}></Route>
         <Route exact path="/registro" element={<Registro></Registro>}></Route>
-        <Route exact path="/login" element={<Login setUsuarioLogueado={setUsuarioLogueado}></Login>}></Route>
+        <Route
+          exact
+          path="/login"
+          element={<Login setUsuarioLogueado={setUsuarioLogueado}></Login>}
+        ></Route>
         <Route
           exact
           path="/detalle"
           element={<DetalleProducto></DetalleProducto>}
         ></Route>
         <Route
-          exact
-          path="/administrador"
-          element={<Administrador></Administrador>}
-        ></Route>
-        <Route
-          exact
-          path="/administrador/crear-producto"
-          element={<CrearProducto></CrearProducto>}
-        ></Route>
-        <Route
-          exact
-          path="/administrador/editar-producto"
-          element={<EditarProducto></EditarProducto>}
+          path="/administrador/*"
+          element={
+            <RutasProtegidas>
+              <RutasAdministrador></RutasAdministrador>
+            </RutasProtegidas>
+          }
         ></Route>
         <Route path="*" element={<Error404></Error404>}></Route>
       </Routes>
